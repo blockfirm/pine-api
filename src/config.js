f9a38c4dda53f1498d400309ddcf55fde4a93b05
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-12-01 14:05:08 +0100

    Remove fee settings from config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 const config = {
   bitcoin: {
     network: 'testnet', // 'mainnet' or 'testnet'
-    fee: {
-      // The maximum number of blocks a transaction should have to wait before it is
-      // predicted to be included in a block.
-      numberOfBlocks: 3
-    },
     btcd: {
       uri: 'ws://127.0.0.1:18334/ws',
       username: '',
