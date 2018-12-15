cd351200f4b7a50fac4ee5d2338f8da2b7e2c5c4
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-12-16 00:07:05 +0100

    Create a RedisClient

--- src/createGlobalContext.js
+++ src/createGlobalContext.js
@@ -1,29 +1,8 @@
-import redis from 'redis';
+import { RedisClient } from './redis';
 
 const createGlobalContext = (config) => {
-  const redisClient = redis.createClient(
-    config.redis.port,
-    config.redis.host,
-    {
-      // eslint-disable-next-line camelcase
-      retry_strategy: () => 2000 // Try to reconnect after 2 seconds.
-    }
-  );
-
-  redisClient.on('connect', () => {
-    console.log('[REDIS] ✅ Connected');
-  });
-
-  redisClient.on('reconnecting', () => {
-    console.log('[REDIS] ♻️  Reconnecting...');
-  });
-
-  redisClient.on('error', (error) => {
-    console.error('[REDIS] 🔥 Error: ', error.message);
-  });
-
   const context = {
-    redis: redisClient,
+    redis: new RedisClient(config.redis),
     config
   };
 
---
+++ src/redis/RedisClient.js
@@ -0,0 +1,55 @@
+import redis from 'redis';
+
+export default class RedisClient {
+  constructor(config) {
+    this.config = config;
+    this._connect();
+  }
+
+  _connect() {
+    this.client = redis.createClient(
+      this.config.port,
+      this.config.host,
+      {
+        // eslint-disable-next-line camelcase
+        retry_strategy: () => 2000 // Try to reconnect after 2 seconds.
+      }
+    );
+
+    this.client.on('connect', () => {
+      console.log('[REDIS] ✅ Connected');
+    });
+
+    this.client.on('reconnecting', () => {
+      console.log('[REDIS] ♻️  Reconnecting...');
+    });
+
+    this.client.on('error', (error) => {
+      console.error('[REDIS] 🔥 Error: ', error.message);
+    });
+  }
+
+  get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve(result);
+      });
+    });
+  }
+
+  set(key, value) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve(result);
+      });
+    });
+  }
+}
