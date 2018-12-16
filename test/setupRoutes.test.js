bf1553bde5100e3f9e7bd7b5faa85bfb8af4f219
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-12-16 23:02:43 +0100

    Test that the subscription endpoints are created

--- test/setupRoutes.test.js
+++ test/setupRoutes.test.js
@@ -64,6 +64,27 @@ describe('setupRoutes.js', () => {
       assert(fakeServer.post.calledWithMatch('/v1/bitcoin/transactions'));
     });
 
+    it('registers the route POST /v1/bitcoin/subscriptions', () => {
+      setupRoutes(fakeServer);
+
+      assert(fakeServer.post.called);
+      assert(fakeServer.post.calledWithMatch('/v1/bitcoin/subscriptions'));
+    });
+
+    it('registers the route GET /v1/bitcoin/subscriptions/:id', () => {
+      setupRoutes(fakeServer);
+
+      assert(fakeServer.get.called);
+      assert(fakeServer.get.calledWithMatch('/v1/bitcoin/subscriptions/:id'));
+    });
+
+    it('registers the route DELETE /v1/bitcoin/subscriptions/:id', () => {
+      setupRoutes(fakeServer);
+
+      assert(fakeServer.del.called);
+      assert(fakeServer.del.calledWithMatch('/v1/bitcoin/subscriptions/:id'));
+    });
+
     it('wraps each endpoint with wrapEndpoint()', () => {
       setupRoutes(fakeServer);
       assert.equal(wrapEndpointSpy.callCount, 8);
