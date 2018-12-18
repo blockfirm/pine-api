05fe0429f7cde289cb96084cf454ebc5d1dbf9b2
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-12-18 01:27:27 +0100

    Remove unnecessary promise

--- src/endpoints/info/get.js
+++ src/endpoints/info/get.js
@@ -1,26 +1,24 @@
 import { HttpInternalServerError } from '../../errors';
 
 const get = function get(request, response) {
-  return Promise.resolve().then(() => {
-    return this.btcd.getInfo()
-      .then((info) => {
-        response.send({
-          isConnected: true,
-          network: info.testnet ? 'testnet' : 'mainnet',
-          blocks: info.blocks
-        });
-      })
-      .catch(() => {
-        response.send({
-          isConnected: false,
-          network: this.config.bitcoin.network,
-          blocks: 0
-        });
-      })
-      .catch((error) => {
-        throw new HttpInternalServerError(error.message);
+  return this.btcd.getInfo()
+    .then((info) => {
+      response.send({
+        isConnected: true,
+        network: info.testnet ? 'testnet' : 'mainnet',
+        blocks: info.blocks
       });
-  });
+    })
+    .catch(() => {
+      response.send({
+        isConnected: false,
+        network: this.config.bitcoin.network,
+        blocks: 0
+      });
+    })
+    .catch((error) => {
+      throw new HttpInternalServerError(error.message);
+    });
 };
 
 export default get;
