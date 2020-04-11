6dca6501315dcc77c0aa8484b88a8ae2ef03da65
Timothy Engqvist Johansson <timothy@blockfirm.se>
2020-04-11 15:58:10 +0200

    Return generic 500 errors for unexpected server errors

--- src/handleError.js
+++ src/handleError.js
@@ -1,10 +1,14 @@
 const handleError = (error, response) => {
   const status = error.status || 500;
-  const message = error.message || 'Unknown error';
+  let message = error.message || 'Unknown error';
+
+  if (status === 500) {
+    message = 'An unexpected error occurred on the server';
+  }
 
   response.send(status, { error: message });
 
-  console.error(`[API] ⛔️ ${status} ${message}`);
+  console.error(`[API] ERROR ${status}: ${error.message}`);
 };
 
 export default handleError;
